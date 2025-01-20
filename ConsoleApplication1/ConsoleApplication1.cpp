28b94407fe01640b1a53cc99118eb99524f0f438
aleksandr.galiew@gmail.com <aleksandr.galiew@gmail.com>
2025-01-20 15:23:46 +0300

    save

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <thread>
+#include <vector>
+
 #include "SimpleTimer.h"
 
 using namespace std;
@@ -17,16 +19,49 @@ inline void ttsleep(float t) { this_thread::sleep_for(chrono::milliseconds((int)
 #pragma endregion HEAD
 
 
+struct Gavka {
+    Gavka()                          { wl("+G"); }
+    Gavka(string name) : _name(name) { wl("+G"); call(); }
+    ~Gavka()                         { wl("-G"); }
+
+    void call() { w("Howdy, "); w(_name); wl("!"); }
+
+private:
+    string _name;
+
+};
+
+
+
+
 #pragma region int
 int main() {
     setlocale(LC_ALL, "RU_ru");
     system("color 70\n cls\n");
     SimpleTimer STimer;
 #pragma endregion main()
-
-   
-
     
+    vector <Gavka*> Psy;
+    Psy.push_back(new Gavka("Aleksandr"));
+    Psy.push_back(new Gavka("Aleksey"));
+    Psy.push_back(new Gavka("Alesha"));
+    Psy.push_back(new Gavka("Anatoliy"));
+    
+    wl();    
+    for (auto _ : Psy) _->call();
+    wl();
+
+    Psy.pop_back();
+    for (auto _ : Psy) _->call();
+    wl();
+
+    Psy.erase(Psy.cbegin() + 1);
+    for (auto _ : Psy) _->call();
+    wl();
+
+
+
+    Psy.clear();
 
 #pragma region }
     std::cout << std::endl << std::endl;
